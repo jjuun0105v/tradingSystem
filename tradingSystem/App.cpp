62fd969c9d5dedf1d86752d03090bfcead51d9f5
juwon.kim <saylily90@naver.com>
2024-07-05 14:19:51 +0900

    * tc added - not allowed to buy 0 stock - not allowed to sell 0 stock - exception could be changed by anyone

--- tradingSystem/App.cpp
+++ tradingSystem/App.cpp
@@ -1,12 +1,26 @@
-
+#include <stdexcept>
 using namespace std;
+
+class BuyZeroFailException : public std::exception
+{
+public:
+    char const* what() const override
+    {
+        return "buy zero not allowed !!!!";
+    }
+};
+
+class SellZeroFailException : public std::exception
+{
+public:
+    char const* what() const override
+    {
+        return "sell zero not allowed !!!!";
+    }
+};
 
 class App
 {
-public:
-
-
-private:
 
 
 };
