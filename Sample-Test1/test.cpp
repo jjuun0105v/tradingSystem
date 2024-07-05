d8614add06337138116293cb4d54546bf3a4a4f3
juwon.kim <saylily90@naver.com>
2024-07-05 13:28:49 +0900

    file added

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,11 @@
+//#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+using namespace std;
+using namespace testing;
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
